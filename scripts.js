a4a86dbbfdaf6e758a383ed058e36f5b7d0cd73f
izek-izzy <izekbanda92@gmail.com>
2024-11-29 12:09:02 +0200

    Update : include compChoice variable to convert the getComputerChoice function into a string(Rock, Paper or Scissors)

--- scripts.js
+++ scripts.js
@@ -2,3 +2,14 @@ function getComputerChoice(max){
     return Math.floor(Math.random() * max);
 }
 
+let compChoice;
+if (getComputerChoice(3)===0){
+    compChoice='Rock';
+    console.log('Computer chose Rock!')
+}else if(getComputerChoice(3)===1){
+    compChoice='Paper';
+    console.log('Computer chose Paper!')
+}else{
+    compChoice='Scissors';
+    console.log('Computer chose Scissors');
+}
